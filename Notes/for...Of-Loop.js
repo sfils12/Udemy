4a1273e0a320d8475daff10b2e6d8cf19987f7a8
Stanley Fils <stanleyfils@gmail.com>
2020-02-07 21:41:44 -0500

    For...Of loop notes

---
+++ Notes/for...Of-Loop.js
@@ -0,0 +1,52 @@
+let subreddits = ['soccer', 'popheads', 'cringe', 'books'];
+// With a standard for loop
+for (let i = 0; i < subreddits.length; i++) {
+	console.log(subreddits[i]);
+}
+//Much cleaner  with a for...of loop!
+for (let sub of subreddits) {
+	console.log(sub);
+}
+//Works with other iterables, like strings!
+for (let char of 'cockadoodledoo') {
+	console.log(char.toUpperCase());
+}
+
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-
+
+const magicSquare = [
+	[2, 7, 6],
+	[9, 5, 1],
+	[4, 3, 8]
+];
+
+// Version using a for loop...
+for (let i = 0; i < magicSquare.length; i++) {
+	let row = magicSquare[i];
+	let sum = 0;
+	for (let j = 0; j < row.length; j++) {
+		sum += row[j];
+	}
+	console.log(`${row} summed to ${sum}`);
+}
+
+// Much cleaner version using a for...of
+for (let row of magicSquare) {
+	let sum = 0;
+	for (let num of row) {
+		sum += num;
+	}
+	console.log(`${row} summed to ${sum}`);
+}
+
+// EXAMPLE 2
+// If you need the indices, use a traditional for loop!
+const words1 = ['mail', 'milk', 'bath', 'black'];
+const words2 = ['box', 'shake', 'tub', 'berry'];
+
+for (let i = 0; i < words1.length; i++) {
+	//Access index i of both arrays
+	console.log(`${words1[i]}${words2[i]}`);
+}
