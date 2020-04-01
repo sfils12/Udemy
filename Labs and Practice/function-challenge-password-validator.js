2b5fcf8847ab661af1fe57a48f57c462a73bc36d
Stanley Fils <stanleyfils@gmail.com>
2020-04-01 18:11:52 -0400

    Practice validating passwords

---
+++ Labs and Practice/function-challenge-password-validator.js
@@ -0,0 +1,76 @@
+// Write a isValidPassword function
+// It accepts 2 arguments: password and username
+// Password must:
+//	- be at least 8 characters
+//  - cannot contain spaces
+//  - cannot contain the username
+// If all requirements are met, return true.
+//Otherwise: false
+
+// isValidPassword('89Fjj1nms', 'dogLuvr');  //true
+// isValidPassword('dogLuvr123!', 'dogLuvr') //false
+// isValidPassword('hello1', 'dogLuvr') //false
+
+// SOLUTIONS:
+
+function isValidPassword(password, username) {
+    if (password.length < 8) {
+        return false;
+    }
+    if (password.indexOf(' ') !== -1) {
+        return false;
+    }
+    if (password.indexOf(username) !== -1) {
+        return false;
+    }
+    return true;
+}
+
+//OR
+
+function isValidPassword(password, username) {
+    if (
+        password.length < 8 ||
+        password.indexOf(' ') !== -1 ||
+        password.indexOf(username) !== -1
+    ) {
+        return false;
+    }
+    return true;
+}
+
+
+
+//OR
+
+
+function isValidPassword(password, username) {
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+    if (tooShort || hasSpace || tooSimilar) return false;
+    return true;
+}
+
+
+//OR
+
+
+function isValidPassword(password, username) {
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+    if (!tooShort && !hasSpace && !tooSimilar) return true;
+    return false;
+}
+
+
+//OR
+
+
+function isValidPassword(password, username) {
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+    return !tooShort && !hasSpace && !tooSimilar;
+}
